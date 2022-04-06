24d826f0385a60555f2291545380c3f1a67e1276
tamramalho <ramalhothamara1@gmail.com>
2022-04-05 22:16:39 -0300

    Projeto iniciado

---
+++ src/main/java/com/tamara/letvet/LetvetApplication.java
@@ -0,0 +1,13 @@
+package com.tamara.letvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetvetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamara/letvet/LetvetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamara.letvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
